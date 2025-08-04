e23c363f498c3489f6792e06767ce78a66d93fdc
Donovan Capsimalis <donovan.capsimalis@gmail.com>
2025-08-03 18:37:32 -0700

    First working tool

--- Tools/Col_Comparison.py
+++ Tools/Col_Comparison.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as pd
+import matplotlib.pyplot as plt
 import sys
 
 
-data_set1 = pd.readcsv(sys.argv[1])
-data_set2 = pd.readcsv(sys.argv[2])
+data_set1 = pd.read_csv(sys.argv[1])
+data_set2 = pd.read_csv(sys.argv[2])
 
 data_set1 = np.array(data_set1)
 data_set2 = np.array(data_set2)
@@ -13,19 +13,31 @@ data_set2 = np.array(data_set2)
 if np.shape(data_set1) != np.shape(data_set2):
     if len(sys.argv) <= 4:
         print('Data Sets of differing shape')
-        exit()
+        quit()
 
 # calc the basic amount of all diffs for either all columns or a selection of columns
 if len(sys.argv) <= 4:
-    cols = np.range(len(data_set1))
+    cols = range(data_set1.shape[0])
 else:
     cols = sys.argv[4:]
 
 diffs = np.array([data_set1[i] - data_set2[i] for i in cols])
 
+def check_actual_type(val: str):
+    try:
+        float(val)
+        return True
+    except ValueError:
+        return False
 
-for diff in diffs:
-    threshold = 0.01      if sys.argv[3] == "percent" else threshold = 1 if sys.argv[3] == "absolute" else sys.argv[3]
-    diff = diff/data_set1 if sys.argv[3] == "percent" else diff
-    if diff >= threshold:
-        print(diff)
+exceeds = []
+for i, diff in enumerate(diffs):
+    if len(sys.argv) > 3:
+        threshold = 0.01 if sys.argv[3] == "percent" else sys.argv[3] if check_actual_type(sys.argv[3]) else 10
+        diff = diff/data_set1*100 if sys.argv[3] == "percent" else diff
+    else:
+        threshold = 10
+    if (np.abs(diff) >= threshold).any():
+        exceeds.append(i)
+[print(f'Column {i} has differences exceeding threshold {threshold}') for i in exceeds]
+        
